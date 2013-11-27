26cc561d5d1b43ccc8374648f993b43ed10b7ec6
William Waites <ww@eris.okfn.org>
2013-11-27 01:17:47 +0100

    rt runs

--- rt/RT_SiteConfig.pm
+++ rt/RT_SiteConfig.pm
@@ -1,4 +1,6 @@
 Set( $rtname, "example.org" );
 Set( $WebPort, 8080 );
 Set( $WebDomain, "rt.example.org" );
+Set( $DatabaseHost, $ENV{DB_PORT_5432_TCP_ADDR} );
+Set( $DatabasePassword, $ENV{RT_DATABASE_PW} );
 1;
