b41ea901349a9e57ca6e943729ed6f808756deed
William Waites <ww@eris.okfn.org>
2013-12-05 15:10:43 +0100

    mergeusers extension RT#198

--- rt/build_rt.sh
+++ rt/build_rt.sh
@@ -6,3 +6,16 @@ tar -C /src -xzpvf /src/${RTSRC}
 cd /src/${RT} && ./configure --with-db-type=Pg
 make -C /src/${RT} testdeps
 make -C /src/${RT} install
+
+install_ext() {
+	REPO=$1
+	cd /src
+	git clone ${REPO}
+	cd `basename ${REPO}`
+	perl Makefile.PL
+	make
+	make install
+}
+
+install_ext https://github.com/bestpractical/rt-extension-mergeusers
+
